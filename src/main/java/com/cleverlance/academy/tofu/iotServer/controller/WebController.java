b9d30f195405d79203276d98c4cfe9d87d31f56c
hulkato <tomas.hulka@i.cz>
2022-03-24 12:12:11 +0100

    paging on FE implemented and polished

--- src/main/java/com/cleverlance/academy/tofu/iotServer/controller/WebController.java
+++ src/main/java/com/cleverlance/academy/tofu/iotServer/controller/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -21,17 +22,42 @@ public class WebController {
         this.meteorologicalDataService = meteorologicalDataService;
     }
 
-    @GetMapping("/home")
-    public ModelAndView home() {
+
+    @GetMapping("")
+    public ModelAndView home(@RequestParam(value="pageSize",required=false) Integer pageSize, @RequestParam(value="pageNumber",required=false) Integer pageNumber) {
+        if (pageSize == null){
+            pageSize = 10;
+        }
+        if (pageNumber == null){
+            pageNumber = 0;
+        }
+
+        String urlPaginationParams = String.format("?pageSize=%s&pageNumber=%d", pageSize, pageNumber);
+
+        ModelAndView model = new ModelAndView("redirect:/show-data" + urlPaginationParams);
+        return model;
+    }
+
+    @GetMapping("/show-data")
+    public ModelAndView homeRedirect(@RequestParam(value="pageSize",required=false) Integer pageSize, @RequestParam(value="pageNumber",required=false) Integer pageNumber) {
+        if (pageSize == null){
+            pageSize = 10;
+        }
+        if (pageNumber == null){
+            pageNumber = 0;
+        }
+
+        String urlPaginationParams = String.format("?pageSize=%s&pageNumber=%d", pageSize, pageNumber);
+
         ModelAndView model = new ModelAndView("home");
         model.addObject("meteorologicaldata", new MeteorologicalData());
-        model.addObject("meteorologicaldatarecords", this.meteorologicalDataService.getMeteorologicalData(10,0));
+        model.addObject("meteorologicaldatarecords", this.meteorologicalDataService.getMeteorologicalData(pageSize,pageNumber));
         return model;
     }
 
     @PostMapping("/submit-form")
     public ModelAndView submitForm(@ModelAttribute MeteorologicalData meteorologicalData){
-        ModelAndView model = new ModelAndView("redirect:/home");
+        ModelAndView model = new ModelAndView("redirect:");
         this.meteorologicalDataService.saveMeteorologicalData(meteorologicalData);
         return model;
     }
