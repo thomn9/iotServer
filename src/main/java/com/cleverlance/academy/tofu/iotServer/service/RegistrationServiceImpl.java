d511d3e3f06530cb50dc94b1dc42729e73d0c582
hulkato <tomas.hulka@i.cz>
2022-03-29 16:54:54 +0200

    base for typed client

--- src/main/java/com/cleverlance/academy/tofu/iotServer/service/RegistrationServiceImpl.java
+++ src/main/java/com/cleverlance/academy/tofu/iotServer/service/RegistrationServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cleverlance.academy.tofu.iotServer.service;
 
 
-import com.cleverlance.academy.aggregator.client.api.IdentificationApi;
+import com.cleverlance.academy.tofu.iotServer.client.api.IdentificationApi;
+import com.cleverlance.academy.tofu.iotServer.client.model.Identification;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
 
-    private final com.cleverlance.academy.aggregator.client.api.IdentificationApi identificationApi;
+    private final IdentificationApi identificationApi;
 
     public RegistrationServiceImpl(IdentificationApi identificationApi) {
         this.identificationApi = identificationApi;
@@ -16,6 +21,18 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public void registerToAggregatorOnStartUp() {
+
+        List<Identification> response = (List<Identification>) this.identificationApi.getIdentifications1(10,0);
+        StringBuilder sb = new StringBuilder();
+        for (Identification s : response)
+        {
+            sb.append(s.toString());
+            sb.append("\t");
+        }
+
+
+        log.info("Reply from aggregator {}", sb.toString() );
+        ;
 
     }
 }
