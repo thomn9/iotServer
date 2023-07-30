5e8560b5553107faa4c05116e105dea56f1e43e6
hulkato <tomas.hulka@i.cz>
2023-07-30 17:46:21 +0200

    update

--- src/main/java/cz/reservation/app/controller/ReservableScheduleWebSocketController.java
+++ src/main/java/cz/reservation/app/controller/ReservableScheduleWebSocketController.java
@@ -5,6 +5,8 @@ import cz.reservation.app.model.WsRequest;
 import cz.reservation.app.model.dto.ReservableScheduleBaseDto;
 import cz.reservation.app.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
