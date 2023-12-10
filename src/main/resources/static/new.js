f3e4c43fa6c49585cef045d5da84dba8c3395cf8
hulkato <tomas.hulka@i.cz>
2023-12-10 16:43:29 +0100

    update

--- src/main/resources/static/new.js
+++ src/main/resources/static/new.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
     $('.datepicker').datepicker({
         format: 'dd-mm-yyyy',
         autoclose: true,
-        startDate: '0d'
+        startDate: '0d',
+        weekStart: 1
+    });
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const reservationDate = urlParams.get('reservationDate');
+    $('.datepicker').datepicker('setDate', new Date(reservationDate));
+    $('.datepicker').datepicker('update');
+
+
+    $('.datepicker').datepicker()
+        .on('changeDate', function(e) {
+        urlParams.set('reservationDate',`${e.date.getFullYear()}-${e.date.getMonth()+1}-${e.date.getDate()}`);
+
+        window.history.replaceState({}, '', `${location.pathname}?${urlParams}`);
+
     });
 
     $('.cell').click(function(){
